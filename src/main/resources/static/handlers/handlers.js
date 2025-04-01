d1fa28285b7c7a62421acff19f4c9c1375783a23
SpeccyPlays <lilben13@googlemail.com>
2025-04-01 22:34:34 +0100

    Updated year on click handler and classes for year divs

--- src/main/resources/static/handlers/handlers.js
+++ src/main/resources/static/handlers/handlers.js
@@ -1,13 +1,13 @@
 function handleYearClick(event, year) {
     event.preventDefault(); // Stops the default link behavior
     const yearDiv = `divyear_${year}`;
-    const allYearDivs = document.querySelectorAll("div");
+    const allYearDivs = document.querySelectorAll(".divyearcontainer");
     console.log("Year div: ", yearDiv);
     console.log(allYearDivs);
     allYearDivs.forEach(div =>{
         console.log(div);
         const displayValue = div.id === yearDiv ? "block" : "none"
-        div.style.display = displayValue; 
+        div.style.display = displayValue;
     });
 }
 function handleMonthClick(event, month) {
